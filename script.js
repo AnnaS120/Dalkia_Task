028fc21586cca836c76877423ccdcfaa2984518e
AnnaS120 <anneyx777@gmail.com>
2025-04-07 20:53:10 +0100

    Refactored JavaScript code

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-function f1() {
-    var p = new Date();
-    var q = new Date("2025-12-25");
-  
-    var variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
-  
-    var variable2 = document.getElementById("my-element");
-    variable2.innerText = variable;
-  }
-  
-  function f2() {
-    var p = new Date();
-    var q = new Date("2026-01-01");
-  
-    var variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
-  
-    var variable2 = document.getElementById("my-element2");
-    variable2.innerText = variable;
-  }
-  
-  f1();
-  f2();
+// Calculating days until a specific date
+function calculateDaysUntil(specificDate, elementId) {
+    const currentDate = new Date();
+    const target = new Date(specificDate);
+    const daysUntil = Math.round((target - currentDate) / (1000 * 60 * 60 * 24));
+
+    const element = document.getElementById(elementId);
+    element.innerText = daysUntil;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    calculateDaysUntil("2025-12-25", "my-element");  // Days until Christmas
+    calculateDaysUntil("2026-01-01", "my-element2");  // Days until New Year
+  });
