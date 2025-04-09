8ee474ad5f2f3fd55227f928cfc8841f77919e5b
AnnaS120 <anneyx777@gmail.com>
2025-04-09 11:45:10 +0100

    Take 2

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
+const CHRISTMAS_DATE = "2025-12-25";
+const NEW_YEAR_DATE = "2026-01-01";
+
 // Calculating days until a specific date
-function calculateDaysUntil(specificDate, elementId) {
+function calculateDaysUntil(specificDate) {
     const currentDate = new Date();
     const target = new Date(specificDate);
-    const daysUntil = Math.round((target - currentDate) / (1000 * 60 * 60 * 24));
-
-    const element = document.getElementById(elementId);
-    element.innerText = daysUntil;
+    return Math.round((target - currentDate) / (1000 * 60 * 60 * 24));
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    calculateDaysUntil("2025-12-25", "my-element");  // Days until Christmas
-    calculateDaysUntil("2026-01-01", "my-element2");  // Days until New Year
-  });
+// Updating DOM with a given value
+function updateElementTextById(id, text) {
+    const element = document.getElementById(id);
+
+    if (!element) {
+        console.error(`Countdown not available (Element with ID "${id}" not found.)`);
+        return;
+    }
+
+    element.innerText = text;
+}
+// Updating countdowns
+function updateCountdowns() {
+    const christmasDays = calculateDaysUntil(CHRISTMAS_DATE);
+    updateElementTextById("my-element", christmasDays);
+
+    const newYearDays = calculateDaysUntil(NEW_YEAR_DATE);
+    updateElementTextById("my-element2", newYearDays);
+}
+
+document.addEventListener("DOMContentLoaded", updateCountdowns);
+
+
+
+
